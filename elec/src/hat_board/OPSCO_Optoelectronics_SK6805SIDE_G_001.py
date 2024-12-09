1d16407ed141e3519322b483310b12938fb178e3
napowderly <napowderly@gmail.com>
2024-12-09 00:00:44 -0800

    50% layout for hat

--- elec/src/hat_board/OPSCO_Optoelectronics_SK6805SIDE_G_001.py
+++ elec/src/hat_board/OPSCO_Optoelectronics_SK6805SIDE_G_001.py
@@ -5,6 +5,7 @@ import logging
 
 import faebryk.library._F as F  # noqa: F401
 from faebryk.core.module import Module
+from faebryk.core.moduleinterface import ModuleInterface
 from faebryk.libs.library import L  # noqa: F401
 from faebryk.libs.picker.picker import DescriptiveProperties
 from faebryk.libs.units import P  # noqa: F401
@@ -12,7 +13,7 @@ from faebryk.libs.units import P  # noqa: F401
 logger = logging.getLogger(__name__)
 
 
-class OPSCO_Optoelectronics_SK6805SIDE_G_001(Module):
+class _OPSCO_Optoelectronics_SK6805SIDE_G_001(Module):
     """
     TODO: Docstring describing your module
 
@@ -63,3 +64,36 @@ class OPSCO_Optoelectronics_SK6805SIDE_G_001(Module):
         #          parametrization
         # ------------------------------------
         pass
+
+class SK6805Interface(ModuleInterface):
+    power: F.ElectricPower
+    data: F.ElectricLogic
+
+
+class OPSCO_Optoelectronics_SK6805SIDE_G_001(Module):
+    """
+    SMD,1.3x3.5mm RGB LEDs(Built-in IC) ROHS
+    """
+    input: SK6805Interface
+    output: SK6805Interface
+
+    led: _OPSCO_Optoelectronics_SK6805SIDE_G_001
+
+    @L.rt_field
+    def can_bridge(self):
+        return F.can_bridge_defined(self.input, self.output)
+
+    @L.rt_field
+    def single_electric_reference(self):
+        return F.has_single_electric_reference_defined(self.input.power)
+
+    def __preinit__(self):
+        self.input.power.connect(self.output.power)
+        self.input.power.hv.connect(self.led.VDD)
+        self.led.VSS.connect(self.input.power.lv)
+        self.led.DOUT.connect(self.output.data.signal)
+        self.led.DIN.connect(self.input.data.signal)
+
+        input_cap = self.input.power.decoupled.decouple()
+        input_cap.capacitance.constrain_subset(L.Range.from_center_rel(100 * P.nF, 0.2))
+        input_cap.add(F.has_package_requirement("0402"))
--- elec/src/hat_board/hat_board.py
+++ elec/src/hat_board/hat_board.py
@@ -9,7 +9,8 @@ from faebryk.libs.library import L  # noqa: F401
 from faebryk.libs.units import P  # noqa: F401
 
 from .nfc.NXP_Semicon_PN5321A3HN_C106_51 import NXP_Semicon_PN5321A3HN_C106_51
-from .TE_Connectivity_1_2328702_0 import TE_Connectivity_1_2328702_0
+from .TE_Connectivity_1_2328702_0 import BoardToBoardConnector
+from .OPSCO_Optoelectronics_SK6805SIDE_G_001 import OPSCO_Optoelectronics_SK6805SIDE_G_001
 # from .capacative_sensor.
 
 logger = logging.getLogger(__name__)
@@ -18,12 +19,37 @@ class HatBoard(Module):
     """
     HAT Board
     """
+    power_3v3: F.ElectricPower
+    power_5v: F.ElectricPower
+    i2c: F.I2C
+
     nfc: NXP_Semicon_PN5321A3HN_C106_51
-    board_to_board_connector: TE_Connectivity_1_2328702_0
+    board_to_board_connector: BoardToBoardConnector
+    leds = L.list_field(23, OPSCO_Optoelectronics_SK6805SIDE_G_001)
     # capacitive_sensor: 
 
 
     def __preinit__(self):
 
         # Connector inverts pinmap, need to flip it back
-        self.board_to_board_connector.invert_pinmap()
+        self.board_to_board_connector.invert_connector_pinmap()
+        
+        # Connect LEDs in series
+        for i in range(len(self.leds)-1):
+            self.leds[i].output.connect(self.leds[i+1].input)
+
+        # Connect first LED
+        self.power_3v3.connect(self.board_to_board_connector.power_3v3, self.nfc.power_3v3)
+        self.power_5v.connect(self.leds[0].input.power, self.board_to_board_connector.power_5v)
+        self.board_to_board_connector.hat_led_data.connect(self.leds[0].input.data)
+
+        # Net naming
+        F.Net.with_name("led_data").part_of.connect(self.board_to_board_connector.hat_led_data.signal)
+        F.Net.with_name("power_3v3").part_of.connect(self.power_3v3.hv)
+        F.Net.with_name("power_5v").part_of.connect(self.power_5v.hv)
+        F.Net.with_name("gnd").part_of.connect(self.power_3v3.lv)
+        F.Net.with_name("reset").part_of.connect(self.board_to_board_connector.hat_reset.signal)
+        F.Net.with_name("i2c_scl").part_of.connect(self.board_to_board_connector.i2c.scl.signal)
+        F.Net.with_name("i2c_sda").part_of.connect(self.board_to_board_connector.i2c.sda.signal)
+        F.Net.with_name("nfc_irq").part_of.connect(self.board_to_board_connector.hat_nfc_irq.signal)
+        F.Net.with_name("touch_irq").part_of.connect(self.board_to_board_connector.hat_touch_irq.signal)
